ee450e25d3bf04330c3c9942de4a504c31cb517a
Williams <williams.edi@gmail.com>
2023-01-16 08:49:40 -0500

    Added a limelight Subsystem

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,7 +1,33 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.event.NetworkBooleanEvent;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
-    
+
+    private NetworkTable table;
+
+    public LimelightSubsystem() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public double getX() {
+        return table.getEntry("tx").getDouble(0);
+    }
+
+    public double getY() {
+        return table.getEntry("ty").getDouble(0);
+    }
+
+    public double getA() {
+        return table.getEntry("ta").getDouble(0);
+    }
+
+    public long getId() {
+        return table.getEntry("tid").getInteger(0);
+    }
+
 }
