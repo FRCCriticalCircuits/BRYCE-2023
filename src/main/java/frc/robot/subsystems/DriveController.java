8e92a74030ee9aa77d04953b9e0ea59d72251676
Williams <williams.edi@gmail.com>
2023-01-19 10:15:56 -0500

    Added a Drive Controller Class

---
+++ src/main/java/frc/robot/subsystems/DriveController.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveController extends SubsystemBase {
+    private DriveSubsystem drive;
+    
+    public DriveController() {
+        setup();
+    } 
+
+    public void setup(){
+
+    }
+
+    @Override
+    public void periodic(){}
+}
