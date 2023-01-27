f1297db7d3b3bc8a495923b3264b4c774ae5ef30
Williams <williams.edi@gmail.com>
2023-01-27 09:57:14 -0500

    Added Field Oriented Drive

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -8,18 +8,23 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class TeleopDrive extends CommandBase {
   private final DriveSubsystem drive;
   private double x1, x2, y;
+  private double max;
+  private boolean fieldOrientedDrive;
 
-  public TeleopDrive(DriveSubsystem drive, double x1, double x2, double y) {
+  public TeleopDrive(DriveSubsystem drive, double x1, double x2, double y, boolean fieldOrientedDrive) {
     this.drive = drive;
     this.x1 = x1;
     this.x2 = x2;
     this.y = y;
+    this.fieldOrientedDrive = fieldOrientedDrive;
 
     addRequirements(drive);
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+    max = 0;
+  }
 
   @Override
   public void execute() {
@@ -29,7 +34,7 @@ public class TeleopDrive extends CommandBase {
     
     x1 = drive.driverJoystick.getRawAxis(0);
     x2 = drive.driverJoystick.getRawAxis(2);
-    y = -drive.driverJoystick.getRawAxis(1);
+    y = drive.driverJoystick.getRawAxis(1);
     
 
     if(Math.abs(x1) < Constants.OperatorConstants.DRIVER_X1_THRESHOLD){
@@ -44,15 +49,22 @@ public class TeleopDrive extends CommandBase {
 
     //double magnitude = Math.sqrt(Math.pow(x1, 2) + Math.pow(y, 2));
 
-    double a = x1 - x2 * (Constants.PhysicalConstants.SIDE_LENGTH / (Constants.PhysicalConstants.SIDE_LENGTH / Constants.PhysicalConstants.SIDE_TO_CORNER ));
-    double b = x1 + x2 * (Constants.PhysicalConstants.SIDE_LENGTH / (Constants.PhysicalConstants.SIDE_LENGTH / Constants.PhysicalConstants.SIDE_TO_CORNER));
-    double c = y - x2 * (Constants.PhysicalConstants.SIDE_LENGTH / (Constants.PhysicalConstants.SIDE_WIDTH / Constants.PhysicalConstants.SIDE_TO_CORNER));
-    double d = y + x2 * (Constants.PhysicalConstants.SIDE_LENGTH / (Constants.PhysicalConstants.SIDE_WIDTH / Constants.PhysicalConstants.SIDE_TO_CORNER));
+    double a = x1 - x2 * (Constants.PhysicalConstants.SIDE_LENGTH / Constants.PhysicalConstants.SIDE_TO_CORNER );
+    double b = x1 + x2 * (Constants.PhysicalConstants.SIDE_LENGTH / Constants.PhysicalConstants.SIDE_TO_CORNER);
+    double c = y - x2 * (Constants.PhysicalConstants.SIDE_WIDTH / Constants.PhysicalConstants.SIDE_TO_CORNER);
+    double d = y + x2 * (Constants.PhysicalConstants.SIDE_WIDTH / Constants.PhysicalConstants.SIDE_TO_CORNER);
 
     double frontleftspeed = Math.sqrt(Math.pow(b, 2) + Math.pow(c, 2));
     double frontrightspeed = Math.sqrt(Math.pow(b, 2) + Math.pow(d, 2));
     double rearleftspeed = Math.sqrt(Math.pow(a, 2) + Math.pow(c, 2));
     double rearrightspeed = Math.sqrt(Math.pow(a, 2) + Math.pow(d, 2));
+
+    if (max > 1) {
+      frontleftspeed /= max;
+      frontrightspeed /= max;
+      rearrightspeed /= max;
+      rearleftspeed /= max;
+    }
 
     double frontleftangle = Math.atan2(b, c) * 180 / Math.PI;
     double frontrightangle = Math.atan2(b, d) * 180 / Math.PI;
@@ -70,10 +82,26 @@ public class TeleopDrive extends CommandBase {
       drive.rearLeft.setSpeed(rearleftspeed);
       drive.rearRight.setSpeed(rearrightspeed);
 
-      drive.frontLeft.setAngle(frontleftangle);
-      drive.frontRight.setAngle(frontrightangle);
-      drive.rearLeft.setAngle(rearleftangle);
-      drive.rearRight.setAngle(rearrightangle);
+      if(fieldOrientedDrive){
+        drive.frontLeft.setAngle(frontleftangle - drive.getHeading().getDegrees());
+      }else{
+        drive.frontLeft.setAngle(frontleftangle);
+      }
+      if(fieldOrientedDrive){
+        drive.frontRight.setAngle(frontrightangle - drive.getHeading().getDegrees());
+      }else{
+        drive.frontRight.setAngle(frontrightangle);
+      }
+      if(fieldOrientedDrive){
+        drive.rearLeft.setAngle(rearleftangle - drive.getHeading().getDegrees());
+      }else{
+        drive.rearLeft.setAngle(rearleftangle);
+      }
+      if(fieldOrientedDrive){
+        drive.rearRight.setAngle(rearrightangle - drive.getHeading().getDegrees());
+      }else{
+        drive.rearRight.setAngle(rearrightangle);
+      }
 
   }
 
