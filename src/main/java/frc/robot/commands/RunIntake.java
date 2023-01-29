68eb3aebabc7c9aa573716d867d8b0e7f131a62c
Williams <williams.edi@gmail.com>
2023-01-29 11:23:50 -0500

    Added Features to Intake Subsystem

---
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class RunIntake extends CommandBase {
+    private Intake intake;
+
+    public RunIntake(Intake intake) {
+        this.intake = intake;
+
+        addRequirements(intake);
+    }
+
+    @Override
+    public void execute() {
+        intake.runIntake();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+}
