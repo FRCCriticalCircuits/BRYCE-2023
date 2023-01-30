eac9477effa5cc6e7acfe847782cebea9730a53b
Williams <williams.edi@gmail.com>
2023-01-30 10:42:22 -0500

    Added Features | UNTESTED |

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,9 +1,28 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
-    public ArmSubsystem() {}
+    private CANSparkMax window = new CANSparkMax(15, MotorType.kBrushed);
+    private SparkMaxPIDController window_PID;
+
+    public ArmSubsystem() {
+
+    }
+
+    public void setup() {
+        window.restoreFactoryDefaults();
+
+        window_PID = window.getPIDController();
+
+        window.setSmartCurrentLimit(20);
+
+        window.burnFlash();
+    }
 
     @Override
     public void periodic() {}
