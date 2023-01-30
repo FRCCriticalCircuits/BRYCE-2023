b4cfc5f629352cd1a582f037126c10866d15ae4b
Williams <williams.edi@gmail.com>
2023-01-30 10:39:40 -0500

    Added Features to Intake PID

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,12 +1,36 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    public Intake() {
+    private CANSparkMax intake;
+    private SparkMaxPIDController intakePID;
 
+    public Intake() {
+        intake = new CANSparkMax(10, MotorType.kBrushless);
     }
 
+    public void setup() {
+        intake.restoreFactoryDefaults();
+
+        intake.setSmartCurrentLimit(0);
+
+        intakePID = intake.getPIDController();
+
+        intake.burnFlash();
+    }
+
+    public void runIntake() {
+        intake.set(.5);
+    }
+
+    public void stopIntake() {
+        intake.setVoltage(0);
+    }
 
     @Override
     public void periodic(){}
