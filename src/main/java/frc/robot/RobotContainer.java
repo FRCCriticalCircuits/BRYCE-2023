351d9571097e4ba433151e3711528faefc35a688
Williams <williams.edi@gmail.com>
2023-02-13 10:59:24 -0500

    Added Intake Binds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,11 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Triggers.AxisTrigger;
 import frc.robot.commands.DriveWithHeading;
+import frc.robot.commands.RunIntake;
 import frc.robot.commands.TeleopDrive;
+import frc.robot.subsystems.DriveController;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -22,6 +25,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
   private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
+  private final DriveController m_driveController = new DriveController(m_driveSubsystem);
+
+  private final Intake intake = new Intake();
 
   private SendableChooser<Command> autoChooser = new SendableChooser<Command>(); 
 
@@ -41,7 +47,8 @@ public class RobotContainer {
 
     m_driveSubsystem.setDefaultCommand(
       new TeleopDrive(
-        m_driveSubsystem, 
+        m_driveSubsystem,
+        m_driveController,
         m_DRIVER_GAMEPAD.getRawAxis(0), 
         m_DRIVER_GAMEPAD.getRawAxis(2), 
         m_DRIVER_GAMEPAD.getRawAxis(1),
@@ -56,13 +63,15 @@ public class RobotContainer {
 
   private Trigger DRIVER_X_BUTTON = new Trigger(m_driverController.button(3));
   private Trigger DRIVER_LEFT_BUMPER = new Trigger(m_driverController.button(6));
+  private Trigger DRIVER_RIGHT_TRIGGER = new Trigger(m_driverController.button(8));
   private Trigger DRIVER_LEFT_TRIGGER = new Trigger(m_driverController.button(7));
-  private AxisTrigger OPERATOR_X_STICK = new AxisTrigger(m_operatorController.getRawAxis(0));
+  private AxisTrigger OPERATOR_X1_STICK = new AxisTrigger(m_operatorController.getRawAxis(0), Constants.OperatorConstants.OPERATOR_X1_THEESHOLD);
 
   private void configureBindings() {
     DRIVER_X_BUTTON.toggleOnTrue(
       new TeleopDrive(
-        m_driveSubsystem, 
+        m_driveSubsystem,
+        m_driveController,
         m_DRIVER_GAMEPAD.getRawAxis(0), 
         m_DRIVER_GAMEPAD.getRawAxis(2), 
         m_DRIVER_GAMEPAD.getRawAxis(1), 
@@ -71,6 +80,7 @@ public class RobotContainer {
     );
 
     DRIVER_LEFT_TRIGGER.onTrue(new DriveWithHeading(m_driveSubsystem, 0));
+    DRIVER_RIGHT_TRIGGER.onTrue(new RunIntake(intake));
   }
 
   public Command getAutonomousCommand() {
