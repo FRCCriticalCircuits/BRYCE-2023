0a666f6ff245c1a739c027e385c70ca0b9001abd
Williams <williams.edi@gmail.com>
2023-02-13 10:58:39 -0500

    Changed Drice to use Chassis Speeds and Kinematics

--- src/main/java/frc/robot/subsystems/DriveController.java
+++ src/main/java/frc/robot/subsystems/DriveController.java
@@ -1,10 +1,21 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
 
 public class DriveController extends SubsystemBase {
     private DriveSubsystem drive;
-    
+    private double frontleftangle, frontrightangle, rearleftangle, rearrightangle;
+    private double frontleftspeed, frontrightspeed, rearleftspeed, rearrightspeed;
+    private SlewRateLimiter limiter = new SlewRateLimiter(.5);
+    private SwerveDriveKinematics kinematics = Constants.PhysicalConstants.KINEMATICS;
+    private SwerveControllerCommand driveControllerCommand;
+
     public DriveController(DriveSubsystem drive) {
         this.drive = drive;
         setup();
@@ -14,18 +25,19 @@ public class DriveController extends SubsystemBase {
         
     }
 
-    public void translate(double angle, double power) {
-        drive.frontLeft.setAngle(angle);
-        drive.frontRight.setAngle(angle);
-        drive.rearLeft.setAngle(angle);
-        drive.rearRight.setAngle(angle);
+    public void drive(double x1, double y, double x2, boolean fieldOrientedDrive) {
+        new ChassisSpeeds();
+        SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(
+            !fieldOrientedDrive 
+            ? new ChassisSpeeds(x1, y, x2) : 
+            ChassisSpeeds.fromFieldRelativeSpeeds(x1, y, x2, drive.getHeading())
+        );
 
-        drive.frontLeft.setSpeed(power);
-        drive.frontRight.setSpeed(power);
-        drive.rearLeft.setSpeed(power);
-        drive.rearRight.setSpeed(power);
+        drive.setStates(moduleStates);
     }
 
     @Override
-    public void periodic(){}
+    public void periodic(){
+        
+    }
 }
