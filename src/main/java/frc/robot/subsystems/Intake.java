d4f640225073ac9cf75ffd21285f5d1cf1d82f9f
Williams <williams.edi@gmail.com>
2023-02-13 11:01:04 -0500

    Added Second Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,34 +2,44 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private CANSparkMax intake;
+    private CANSparkMax intake1;
+    private CANSparkMax intake2;
     private SparkMaxPIDController intakePID;
 
     public Intake() {
-        intake = new CANSparkMax(10, MotorType.kBrushless);
+        intake1 = new CANSparkMax(20, MotorType.kBrushless);
+        intake2 = new CANSparkMax(21, MotorType.kBrushless);
     }
 
     public void setup() {
-        intake.restoreFactoryDefaults();
+        intake1.restoreFactoryDefaults();
+        intake2.restoreFactoryDefaults();
 
-        intake.setSmartCurrentLimit(0);
+        intake1.setSmartCurrentLimit(0);
 
-        intakePID = intake.getPIDController();
+        intakePID = intake1.getPIDController();
 
-        intake.burnFlash();
+        intake1.setIdleMode(IdleMode.kCoast);
+        intake2.setIdleMode(IdleMode.kCoast);
+
+        intake1.burnFlash();
+        intake2.burnFlash();
     }
 
     public void runIntake() {
-        intake.set(.5);
+        intake1.set(.5);
+        intake2.set(.5);
     }
 
     public void stopIntake() {
-        intake.setVoltage(0);
+        intake1.setVoltage(0);
+        intake2.setVoltage(0);
     }
 
     @Override
