4a7b6f02771fbe5f97c308ca62fe41145460107c
Williams <williams.edi@gmail.com>
2023-02-17 10:54:04 -0500

    Improved Control

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,15 +2,19 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
     private CANSparkMax intake1;
     private CANSparkMax intake2;
-    private SparkMaxPIDController intakePID;
+    private SparkMaxPIDController intake1PID, intake2PID;
+    private SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0, 0);
 
     public Intake() {
         intake1 = new CANSparkMax(20, MotorType.kBrushless);
@@ -21,15 +25,34 @@ public class Intake extends SubsystemBase {
         intake1.restoreFactoryDefaults();
         intake2.restoreFactoryDefaults();
 
-        intake1.setSmartCurrentLimit(0);
+        intake1.setSmartCurrentLimit(20);
+        intake2.setSmartCurrentLimit(20);
 
-        intakePID = intake1.getPIDController();
+        intake1PID = intake1.getPIDController();
+        intake2PID = intake2.getPIDController();
+
+        intake1PID.setP(Constants.PIDConstants.INTAKE_PID0_P, 0);
+        intake1PID.setI(Constants.PIDConstants.INTAKE_PID0_I, 0);
+        intake1PID.setD(Constants.PIDConstants.INTAKE_PID0_D, 0);
+        intake1PID.setFF(Constants.PIDConstants.INTAKE_PID0_F, 0);
+
+        intake2PID.setP(Constants.PIDConstants.INTAKE_PID0_P, 0);
+        intake2PID.setI(Constants.PIDConstants.INTAKE_PID0_I, 0);
+        intake2PID.setD(Constants.PIDConstants.INTAKE_PID0_D, 0);
+        intake2PID.setFF(Constants.PIDConstants.INTAKE_PID0_F, 0);
 
         intake1.setIdleMode(IdleMode.kCoast);
         intake2.setIdleMode(IdleMode.kCoast);
 
         intake1.burnFlash();
         intake2.burnFlash();
+    }
+
+    public void setIntakeSpeed(double speedPerSecond){
+        double arb_ff = feedforward.calculate(speedPerSecond);
+        intake1PID.setReference(speedPerSecond, ControlType.kVelocity, 0, arb_ff);
+        intake2PID.setReference(speedPerSecond, ControlType.kVelocity, 0, arb_ff);
+
     }
 
     public void runIntake() {
@@ -43,5 +66,8 @@ public class Intake extends SubsystemBase {
     }
 
     @Override
-    public void periodic(){}
+    public void periodic(){
+        intake1.getEncoder().getVelocity();
+        intake2.getEncoder().getVelocity();
+    }
 }
