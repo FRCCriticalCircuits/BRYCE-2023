9e03193aace87fea54d2785af12b4f85db053f63
Williams <williams.edi@gmail.com>
2023-02-21 11:44:04 -0500

    Made Fixes and Added NavX

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,8 +9,13 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,39 +27,40 @@ public class DriveSubsystem extends SubsystemBase {
       Constants.MotorIDs.FRONT_LEFT_FORWARD_ID,
       Constants.MotorIDs.FRONT_LEFT_ROTATION_ID,
       Constants.MotorIDs.FRONT_LEFT_CANCODER_ID,
-      57.7,
-      false
+      -52.1,
+      true
     );
 
     public Module frontRight = new Module(
         Constants.MotorIDs.FRONT_RIGHT_FORWARD_ID, 
         Constants.MotorIDs.FRONT_RIGHT_ROTATION_ID,
         Constants.MotorIDs.FRONT_RIGHT_CANCODER_ID,
-        3.6,
-        false
+        -1,
+        true
     );
 
     public Module rearLeft = new Module(
         Constants.MotorIDs.REAR_LEFT_FORWARD_ID, 
         Constants.MotorIDs.REAR_LEFT_ROTATION_ID,
         Constants.MotorIDs.REAR_LEFT_CANCODER_ID,
-        -67,
-        false
+        65,
+        true
     );
 
     public Module rearRight = new Module(
         Constants.MotorIDs.REAR_RIGHT_FORWARD_ID, 
         Constants.MotorIDs.REAR_RIGHT_ROTATION_ID,
         Constants.MotorIDs.REAR_RIGHT_CANCODER_ID,
-        158.3,
-        false
+        -159,
+        true
     );
 
     public Joystick driverJoystick = new Joystick(0);
 
     private ChassisSpeeds sChassisSpeeds = new ChassisSpeeds();
 
-    private AnalogGyro gyro = new AnalogGyro(0);
+    //private ADXRS450_Gyro gyro = new ADXRS450_Gyro(edu.wpi.first.wpilibj.SPI.Port.kOnboardCS0);
+    private AHRS gyro = new AHRS(SerialPort.Port.kUSB);
 
     private SwerveModulePosition[] modulePositions = {
       new SwerveModulePosition(frontLeft.getDistance(), frontLeft.getRotation()),
@@ -69,16 +75,15 @@ public class DriveSubsystem extends SubsystemBase {
     
   public DriveSubsystem() {
     OutputModuleInfo();
+    gyro.calibrate();
     setup();
   }
-
-  /*
+ 
   public void getInstance(){
     if(instance == null){
       instance = new DriveSubsystem();
     }
   }
- */
 
   public void setup(){
     // ------------------
@@ -155,7 +160,11 @@ public class DriveSubsystem extends SubsystemBase {
     rearLeft.reset();
     rearRight.reset();
     odometry.resetPosition(getHeading(), modulePositions, new Pose2d());
-    //gyro.reset();
+    gyro.reset();
+  }
+
+  public void resetGyro() {
+    gyro.reset();
   }
 
   public Rotation2d getHeading() {
@@ -196,6 +205,8 @@ public class DriveSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("FRONT RIGHT DISTANCE", frontRight.getDistance());
     SmartDashboard.putNumber("REAR LEFT DISTANCE", rearLeft.getDistance());
     SmartDashboard.putNumber("REAR RIGHT DISTANCE", rearRight.getDistance());
+
+    SmartDashboard.putNumber("ANGLE", gyro.getAngle());
   }
 
   public void OutputModuleInfo(SwerveModuleState[] states) {
