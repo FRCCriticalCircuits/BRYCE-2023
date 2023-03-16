5bb53c3383a1eebc67a7fdffcc9f78401df1d538
Williams <williams.edi@gmail.com>
2023-03-15 22:09:37 -0400

    Improved Subsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
     private CANSparkMax flywheel_Top, flywheel_buttom;
@@ -18,6 +19,8 @@ public class ShooterSubsystem extends SubsystemBase {
     
 
     public ShooterSubsystem() {
+        flywheel_Top = new CANSparkMax(15, MotorType.kBrushless);
+        flywheel_buttom = new CANSparkMax(16, MotorType.kBrushless);
         setup();
     }
 
@@ -25,8 +28,10 @@ public class ShooterSubsystem extends SubsystemBase {
         flywheel_Top.restoreFactoryDefaults();
         flywheel_buttom.restoreFactoryDefaults();
 
-        flywheel_Top = new CANSparkMax(30, MotorType.kBrushless);
-        flywheel_buttom = new CANSparkMax(31, MotorType.kBrushless);
+        flywheel_buttom.setInverted(true);
+
+        flywheel_Top.setSmartCurrentLimit(40);
+        flywheel_buttom.setSecondaryCurrentLimit(40);
 
         flywheel_TopPID = flywheel_Top.getPIDController();
         flywheel_ButtomPID = flywheel_buttom.getPIDController();
@@ -34,18 +39,18 @@ public class ShooterSubsystem extends SubsystemBase {
         flywheel_TopEncoder = flywheel_Top.getEncoder();
         flywheel_ButtomEncoder = flywheel_buttom.getEncoder();
 
-        flywheel_TopEncoder.setVelocityConversionFactor(4 * Math.PI * (1/12));
-        flywheel_ButtomEncoder.setVelocityConversionFactor(4 * Math.PI * (1/12));
+        flywheel_TopEncoder.setVelocityConversionFactor(1 / (4 * Math.PI * 12));
+        flywheel_ButtomEncoder.setVelocityConversionFactor(1 / (4 * Math.PI * 12));
 
-        flywheel_TopPID.setP(0, 0);
-        flywheel_TopPID.setI(0, 0);
-        flywheel_TopPID.setD(0, 0);
-        flywheel_TopPID.setFF(0, 0);
+        flywheel_TopPID.setP(Constants.PIDConstants.FLYWHEELTOP_PID0_P, 0);
+        flywheel_TopPID.setI(Constants.PIDConstants.FLYWHEELTOP_PID0_I, 0);
+        flywheel_TopPID.setD(Constants.PIDConstants.FLYWHEELTOP_PID0_D, 0);
+        flywheel_TopPID.setFF(Constants.PIDConstants.FLYWHEELTOP_PID0_F, 0);
 
-        flywheel_ButtomPID.setP(0, 0);
-        flywheel_ButtomPID.setI(0, 0);
-        flywheel_ButtomPID.setD(0, 0);
-        flywheel_ButtomPID.setFF(0, 0);
+        flywheel_ButtomPID.setP(Constants.PIDConstants.FLYWHEELBUTTOM_PID0_P, 0);
+        flywheel_ButtomPID.setI(Constants.PIDConstants.FLYWHEELBUTTOM_PID0_I, 0);
+        flywheel_ButtomPID.setD(Constants.PIDConstants.FLYWHEELBUTTOM_PID0_D, 0);
+        flywheel_ButtomPID.setFF(Constants.PIDConstants.FLYWHEELBUTTOM_PID0_F, 0);
 
         flywheel_Top.setIdleMode(IdleMode.kBrake);
         flywheel_buttom.setIdleMode(IdleMode.kBrake);
@@ -54,8 +59,39 @@ public class ShooterSubsystem extends SubsystemBase {
         flywheel_buttom.burnFlash();
     }
 
+    public void setBrake(boolean mode){
+        if(mode){
+            flywheel_Top.setIdleMode(IdleMode.kBrake);
+            flywheel_buttom.setIdleMode(IdleMode.kBrake);
+        }else{
+            flywheel_Top.setIdleMode(IdleMode.kCoast);
+            flywheel_buttom.setIdleMode(IdleMode.kCoast);
+        }
+    }
+
     public void setSpeed(double velocity){
+        if(velocity > Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY){
+            velocity = Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY;
+        };
+
         flywheel_TopPID.setReference(velocity, ControlType.kVelocity);
+        flywheel_ButtomPID.setReference(velocity, ControlType.kVelocity);
+    }
+
+    public void setSpeedTop(double velocity){
+        if(velocity > Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY){
+            velocity = Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY;
+        };
+        
+        flywheel_TopPID.setReference(velocity, ControlType.kVelocity);
+    }
+
+    public void setSpeedButtom(double velocity){
+        if(velocity > Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY){
+            velocity = Constants.PhysicalConstants.FLYWHEEL_MAX_VELOCITY;
+        };
+
+        flywheel_ButtomPID.setReference(velocity, ControlType.kVelocity);
     }
 
     public double getVelocityTop(){
@@ -71,5 +107,4 @@ public class ShooterSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("FLYWHEEL TOP", getVelocityTop());
         SmartDashboard.putNumber("FLYWHEEL BUTTOM", getVelocityButtom());
     }
-    
 }
