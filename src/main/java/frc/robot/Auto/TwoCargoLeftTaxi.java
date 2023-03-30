03805a6aa516374d4c8829bc14953b1be322eb1a
Williams <williams.edi@gmail.com>
2023-03-30 15:19:38 -0400

    Migrated Auto to Path Planner

--- src/main/java/frc/robot/Auto/TwoCargoLeftTaxi.java
+++ src/main/java/frc/robot/Auto/TwoCargoLeftTaxi.java
@@ -1,36 +1,41 @@
 package frc.robot.Auto;
 
+import java.security.InvalidKeyException;
+import java.util.HashMap;
 import java.util.List;
 
-import edu.wpi.first.math.controller.HolonomicDriveController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint;
-import edu.wpi.first.math.trajectory.constraint.TrajectoryConstraint;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import frc.robot.Constants;
 import frc.robot.Util.AutoPIDControllers;
-import frc.robot.subsystems.DriveController;
+import frc.robot.Util.DriveController;
+import frc.robot.commands.AutoIntake;
+import frc.robot.commands.AutoSpinUp;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Sequencer;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class TwoCargoLeftTaxi extends CommandBase {
-    public DriveSubsystem drive;
-    public DriveController drivecontroller;
-    public AutoPIDControllers autopidControllers;
+    private DriveSubsystem drive;
+    private ShooterSubsystem shooter;
+    private Intake intake;
+    private Sequencer sequencer; 
+    private DriveController drivecontroller;
+    private AutoPIDControllers autopidControllers;
 
-    public TwoCargoLeftTaxi(DriveSubsystem drive, DriveController drivecontroller, AutoPIDControllers autoPIDControllers) {
+    public TwoCargoLeftTaxi(DriveSubsystem drive, ShooterSubsystem shooter, Intake intake, Sequencer sequencer, DriveController drivecontroller, AutoPIDControllers autoPIDControllers) {
         this.drive = drive;
+        this.shooter = shooter;
+        this.intake = intake;
+        this.sequencer = sequencer;
         this.drivecontroller = drivecontroller;
         this.autopidControllers = autoPIDControllers;
 
@@ -39,42 +44,37 @@ public class TwoCargoLeftTaxi extends CommandBase {
 
 
     public Command twocargolefttaxi() {
-        TrajectoryConfig config = new TrajectoryConfig(1, 0.5).
-        setKinematics(Constants.PhysicalConstants.KINEMATICS).
-        addConstraint(new SwerveDriveKinematicsConstraint(Constants.PhysicalConstants.KINEMATICS, 1))
-        .setReversed(false).setEndVelocity(0);
+        List<PathPlannerTrajectory> leftTaxi = PathPlanner.loadPathGroup("Left Taxi", 1.2, 0.7);
 
-        Trajectory route = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(0, 0, new Rotation2d(0)), 
-        List.of(
-        ), 
-        new Pose2d(3, 0, Rotation2d.fromDegrees(180)), 
-        config
+        HashMap<String, Command> eventMap = new HashMap<>();
+
+        eventMap.put("MARKER 1", new PrintCommand("Marker 1 Passed"));
+        eventMap.put("IntakeStart", new AutoIntake(intake, sequencer, 2));
+        eventMap.put("ShootSecond", new AutoSpinUp(shooter, sequencer, 0, 1.5, 13));
+
+        SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+            drive::getPose, 
+            drive::setPose, 
+            new PIDConstants(
+                0.4, 
+                0, 
+                0
+            ), 
+            new PIDConstants(
+                0.25, 
+                0, 
+                0
+            ), 
+            drive::OutputChassisSpeeds, 
+            eventMap,
+            true, 
+            drive
         );
 
         return new SequentialCommandGroup(
-            new InstantCommand(
-                drive::reset, 
-                drive
-            ), 
-            new SwerveControllerCommand(
-                route, 
-                drive::getPose, 
-                Constants.PhysicalConstants.KINEMATICS,
-                new HolonomicDriveController(
-                    autopidControllers.CRITICAL_X(),
-                    autopidControllers.CRITICAL_Y(),
-                    new ProfiledPIDController(
-                        autopidControllers.CRITICAL_THETA().getP(), 
-                        autopidControllers.CRITICAL_THETA().getI(), 
-                        autopidControllers.CRITICAL_THETA().getD(), 
-                        new Constraints(1, 0.5)
-                    )
-                ),
-                drive::getHeading,
-                drive::OutputModuleInfo,
-                drive).andThen(drive::stop)
-                );
+            new AutoSpinUp(shooter, sequencer, 0, 1.5, 11),
+            autoBuilder.fullAuto(leftTaxi),
+            new InstantCommand(drive::resetHeading, drive)
+        );
     }
-
 }
