ac97e7b4f521a7d905721c4aa1a8a3607d464856
Williams <williams.edi@gmail.com>
2023-03-30 15:20:40 -0400

    Added Teleop Controls

--- src/main/java/frc/robot/commands/AlignToTarget.java
+++ src/main/java/frc/robot/commands/AlignToTarget.java
@@ -1,10 +1,13 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Util.AutoPIDControllers;
-import frc.robot.subsystems.DriveController;
+import frc.robot.Util.DriveController;
+import frc.robot.Util.GoalType.goalType;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 
@@ -13,8 +16,9 @@ public class AlignToTarget extends CommandBase {
     private DriveController driveController;
     private LimelightSubsystem limelight;
     private boolean isAutonomous;
-    private PIDController controller = new PIDController(0, 0, 0);
+    private PIDController controller;
     private Trigger trigger;
+    private DoubleSupplier _trigger;
 
     public AlignToTarget(DriveSubsystem drive, DriveController driveController, LimelightSubsystem limelight, boolean isAutonomous){
         this.drive = drive;
@@ -35,10 +39,26 @@ public class AlignToTarget extends CommandBase {
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        controller = new PIDController(0.021, 0, 0);
+        //controller.setIntegratorRange(-1.5, 1.5);
+    }
 
     @Override
-    public void execute() {}
+    public void execute() {
+        double x1 = drive.driverJoystick.getLeftX();
+        double y = drive.driverJoystick.getLeftY();
+
+        if(Math.abs(x1) < 0.15) {
+            x1 = 0;
+        }
+
+        if(Math.abs(y) < 0.15) {
+            y = 0;
+        }
+
+        driveController.drive(x1, y, -controller.calculate(limelight.getXOffset(), limelight.getBestGoalOffset()), true);
+    }
 
     @Override
     public void end(boolean interrupted) {}
