d9b37a2bb2cfe7bcfdea213defb6d679cd416474
Williams <williams.edi@gmail.com>
2023-05-18 12:56:45 -0400

    Improved AutoBalance Command

--- src/main/java/frc/robot/Auto/AutoBalanceBackup.java
+++ src/main/java/frc/robot/Auto/AutoBalanceBackup.java
@@ -6,57 +6,40 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Util.DriveController;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class AutoBalance extends CommandBase {
+public class AutoBalanceBackup extends CommandBase {
     private DriveSubsystem drive;
     private DriveController driveController;
     private PIDController controller;
-    private double Output, gain = 1;
-    private boolean isReversed;
+    private double Output;
     private int state = 0;
 
-    public AutoBalance(DriveSubsystem drive, DriveController driveController, boolean isReversed) {
+    public AutoBalanceBackup(DriveSubsystem drive, DriveController driveController) {
         this.drive = drive;
         this.driveController = driveController;
-        this.isReversed = isReversed;
-        this.state = 0;
 
-        addRequirements(drive);
-    }
-
-    public AutoBalance(DriveSubsystem drive, DriveController driveController, boolean isReversed, int state) {
-        this.drive = drive;
-        this.driveController = driveController;
-        this.isReversed = isReversed;
-        this.state = state;
-        
         addRequirements(drive);
     }
 
     @Override
     public void initialize() {
         drive.setBrakeMode(true);
-        controller = new PIDController(0.007, 0, 0);
-        controller.setTolerance(0.7);
-
-        if(isReversed) {
-            gain = -1;
-        }else{
-            gain = 1;
-        }
+        controller = new PIDController(0.0065, 0, 0);
     }
 
     @Override
     public void execute() {
         if(state == 0){
-            driveController.drive(0, gain * 0.2, 0, false);
+            driveController.drive(0, 0.2, 0, false);
         }
         
         if(state > 0){
             
             Output = controller.calculate(drive.getRoll(), 0);
-                Output = Math.signum(Output) * 0.15 * gain;
-                driveController.drive(0,  -controller.calculate(drive.getRoll(), 0), 0, false);
+            if(Math.abs(Output) > 0.18)
+                Output = Math.signum(Output) * 0.15;
+                driveController.drive(0, -controller.calculate(drive.getRoll(), 0), 0, false);
             
+
             //drive.stop();
         }
 
