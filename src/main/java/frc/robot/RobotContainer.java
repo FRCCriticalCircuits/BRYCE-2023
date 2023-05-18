458548a47309299749a5ea45227aafa08bca8205
Williams <williams.edi@gmail.com>
2023-05-18 12:56:14 -0400

    Added More Autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,15 @@
 package frc.robot;
 
 import frc.robot.Auto.AutoBalance;
+import frc.robot.Auto.AutoBalanceBackup;
+import frc.robot.Auto.DoNotUse;
+import frc.robot.Auto.DoNothing;
 import frc.robot.Auto.DriveStraight;
 import frc.robot.Auto.MiddleAuto;
+import frc.robot.Auto.Mobility;
+import frc.robot.Auto.SimpleOneCargoTaxi;
+import frc.robot.Auto.ThreeCargoLeftTaxi;
+import frc.robot.Auto.TwoCargoAndBalanceTaxi;
 import frc.robot.Auto.TwoCargoLeftTaxi;
 import frc.robot.Auto.TwoCargoRightTaxi;
 import frc.robot.Auto.testAuto;
@@ -101,20 +108,25 @@ public class RobotContainer {
       )
     );
 
-    autoChooser.setDefaultOption("NONE", null);
-    autoChooser.addOption("DRIVE STRAIGHT", new DriveStraight(m_driveSubsystem, controllers).driveStaight());
-    autoChooser.addOption("LEFT TAXI", new TwoCargoLeftTaxi(m_driveSubsystem, shooter, intake, sequencer, m_driveController, controllers).twocargolefttaxi());
-    autoChooser.addOption("RIGHT TAXI", new TwoCargoRightTaxi(m_driveSubsystem, shooter, intake, sequencer, controllers).cargorighttaxi());
-    autoChooser.addOption("AUTO CLIMB", new AutoBalance(m_driveSubsystem, m_driveController));
-    autoChooser.addOption("MIDDLE TAXI", new MiddleAuto(m_driveSubsystem, shooter, sequencer, controllers).middletaxi());
+    autoChooser.setDefaultOption("DO NOTHING", new DoNothing(m_driveSubsystem, shooter, sequencer).doNothing());
+    autoChooser.addOption("NONE", null);
+    autoChooser.addOption("LEFT TAXI", new TwoCargoLeftTaxi(m_driveSubsystem, shooter, intake, sequencer, limelight, m_driveController, controllers).twocargolefttaxi());
+    autoChooser.addOption("RIGHT TAXI", new TwoCargoRightTaxi(m_driveSubsystem, shooter, intake, sequencer, limelight, controllers).cargorighttaxi());
+    autoChooser.addOption("AUTO CLIMB", new AutoBalanceBackup(m_driveSubsystem, m_driveController));
+    autoChooser.addOption("MIDDLE TAXI", new MiddleAuto(m_driveSubsystem, shooter, sequencer, m_driveController, controllers).middletaxi());
+    autoChooser.addOption("THREE CARGO LEFT", new ThreeCargoLeftTaxi(m_driveSubsystem, shooter, intake, sequencer, limelight, m_driveController, controllers).threecargolefttaxi());
     autoChooser.addOption("Test Auto", new testAuto(m_driveSubsystem, intake, shooter, sequencer).testAuto());
+    autoChooser.addOption("GOD MODE", new DoNotUse(m_driveSubsystem, shooter, intake, sequencer, limelight).donotuse());
+    autoChooser.addOption("Simple Taxi", new SimpleOneCargoTaxi(m_driveSubsystem, shooter, sequencer).simpleonecargotaxi());
+    autoChooser.addOption("MOBILITY AND BALANCE", new Mobility(m_driveSubsystem, shooter, sequencer, m_driveController).mobility());
+    autoChooser.addOption("PLS Work", new TwoCargoAndBalanceTaxi(m_driveSubsystem, shooter, sequencer, intake, limelight, m_driveController).twocargoandbalancetaxi());
 
     SmartDashboard.putData("AUTO", autoChooser);
   }
 
   private Trigger DRIVER_A_BUTTON = new Trigger(m_driverController::getAButton);
   private Trigger DRIVER_X_BUTTON = new Trigger(m_driverController::getXButton);
-  private Trigger DRIVER_B_BUTTON = new Trigger(m_driverController::getBButton);
+  private Trigger DRIVER_B_BUTTON = new Trigger(() -> m_driverController.getBButton());
   private Trigger DRIVER_Y_BUTTON = new Trigger(m_driverController::getYButton);
   private Trigger DRIVER_LEFT_BUMPER = new Trigger(m_driverController::getLeftBumper);
   private Trigger DRIVER_RIGHT_BUMPER = new Trigger(m_driverController::getRightBumper);
@@ -135,12 +147,25 @@ public class RobotContainer {
       )
     );
 
+    DRIVER_B_BUTTON.onTrue(
+      new TeleopDrive(
+        m_driveSubsystem, 
+        m_driveController, 
+        m_DRIVER_GAMEPAD.getRawAxis(0), 
+        m_DRIVER_GAMEPAD.getRawAxis(2), 
+        m_DRIVER_GAMEPAD.getRawAxis(1),
+        true, 
+        0.5
+        )
+    );
+
     DRIVER_LEFT_TRIGGER.onTrue(new AlignToTarget(m_driveSubsystem, m_driveController, limelight, DRIVER_LEFT_TRIGGER));
-    DRIVER_RIGHT_TRIGGER.debounce(.1).onTrue(new RunIntake(intake, sequencer, false, DRIVER_RIGHT_TRIGGER));
-    DRIVER_RIGHT_BUMPER.debounce(.1).onTrue(new RunIntake(intake, sequencer, true, DRIVER_RIGHT_BUMPER));
+    DRIVER_RIGHT_TRIGGER.debounce(.1).onTrue(new RunIntake(intake, sequencer, shooter, false, DRIVER_RIGHT_TRIGGER));
+    DRIVER_RIGHT_BUMPER.debounce(.1).onTrue(new RunIntake(intake, sequencer, shooter, true, DRIVER_RIGHT_BUMPER));
     DRIVER_START_BUTTON.onTrue(new InstantCommand(m_driveSubsystem::resetHeading));
-    DRIVER_Y_BUTTON.debounce(.2).onTrue(new SpinUp(shooter, sequencer, 6, DRIVER_Y_BUTTON));
-    DRIVER_X_BUTTON.debounce(.2).onTrue(new ShootWithVision(shooter, sequencer, limelight));
+    DRIVER_Y_BUTTON.debounce(.2).onTrue(new SpinUp(shooter, sequencer, 8, DRIVER_Y_BUTTON));
+    DRIVER_X_BUTTON.debounce(.2).onTrue(new ShootWithVision(shooter, sequencer, limelight, DRIVER_X_BUTTON));
+    //DRIVER_B_BUTTON.debounce(.2).onTrue(new SpinUp(shooter, sequencer, 8, DRIVER_X_BUTTON, true));
     DRIVER_LEFT_BUMPER.and(DRIVER_Y_BUTTON).onTrue(new InstantCommand(() -> limelight.setGoalType(goalType.HIGH), limelight));
     DRIVER_LEFT_BUMPER.and(DRIVER_X_BUTTON).onTrue(new InstantCommand(() -> limelight.setGoalType(goalType.MID), limelight));
     DRIVER_LEFT_BUMPER.and(DRIVER_A_BUTTON).onTrue(new InstantCommand(() -> limelight.setGoalType(goalType.LOW), limelight));
